72049609788429bd1278b96c24ba04877a8ed067
Erdi <erdi.doqan@gmail.com>
2014-11-01 19:26:01 +0100

    logo

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -104,7 +104,7 @@ class PostController extends BaseController {
 			
 			$logo = Input::file('logo');
 	        $filename  = $sirket->com_name.'-'.$sirket->id.'.'.$logo->getClientOriginalExtension();
-            Image::make($logo)->save('public/img/logo/'.$filename);
+            Image::make($logo)->save('img/logo/'.$filename);
 	        $logo = 'http://institutional.njepuneere.loc/public/img/logo/'.$filename;
 	        $logo = SirketUser::where('id','=',$id)->update(array('logo' => $logo));
 
