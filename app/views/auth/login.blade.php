db2d5d9b767b37ed9230a691f6078557c11d440d
Erdi <erdi.doqan@gmail.com>
2015-01-22 03:09:07 +0100

    new login

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -1,30 +1,45 @@
 @extends('layouts.admin')
 
 @section('content')
-<section id="login">
-    <div class="container">
-    	<div class="row">
-    	    <div class="col-xs-12">
-        	    <div class="form-wrap">
-                <h1>Log in with your email account</h1>
-                {{ Form::open(array('url' => 'login', 'id'=>'login-form')) }}
-                  <div class="form-group">
-                      <label for="email" class="sr-only">email</label>
-                      {{ Form::text('email', '', array('class' => 'form-control', 'placeholder' => 'name@njepuneere.com')) }}
-                      
-                  </div>
-                  <div class="form-group">
-                      <label for="key" class="sr-only">Password</label>
-                      {{ Form::password('password', array('class' => 'form-control','id'=>'key' ,'placeholder' => 'password')) }}
-                  </div>
-                  <input type="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Log in">
-                {{Form::close()}}
-        	    </div>
-    		</div> <!-- /.col-xs-12 -->
-    	</div> <!-- /.row -->
-    </div> <!-- /.container -->
-</section>
 
+
+<div id="login" class="modal-dialog">
+  <div class="modal-content">
+      <div class="modal-header">
+          <h4 class="modal-title"><img src="/img/logo.png" class="logo-img"></h4>
+      </div>
+      <div class="modal-body">
+          <div class="row">
+              <div class="col-xs-6">
+                  <div class="well">
+                      {{ Form::open(array('url' => 'login', 'id'=>'login-form')) }}
+                      <div class="form-group">
+                          <label for="email" class="sr-only">email</label>
+                          {{ Form::text('email', '', array('class' => 'form-control', 'placeholder' => 'name@njepuneere.com')) }}
+                          
+                      </div>
+                      <div class="form-group">
+                          <label for="key" class="sr-only">Password</label>
+                          {{ Form::password('password', array('class' => 'form-control','id'=>'key' ,'placeholder' => 'password')) }}
+                      </div>
+                      <input type="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Log in">
+                    {{Form::close()}}
+                  </div>
+              </div>
+              <div class="col-xs-6">
+                  <p class="lead">Wellcome to <span class="text-primary">nje</span><span class="text-success">pune</span><span class="text-primary">ere</span></p>
+                  <ul class="list-unstyled" style="line-height: 2">
+                      <li><span class="fa fa-check text-success"></span> See all CVs</li>
+                      <li><span class="fa fa-check text-success"></span> Fastly</li>
+                      <li><span class="fa fa-check text-success"></span> Save your joblist</li>
+                      <li><span class="fa fa-check text-success"></span> checkout all institution</li>
+                      <li><span class="fa fa-check text-success"></span> Get a gift <small>(only new customers)</small></li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
 
 
 <script type="text/javascript">
--- app/views/layouts/admin.blade.php
+++ app/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
 	@include('partials/header')
 	@include('partials/scripts')
   </head>
-  <body>
+  <body style="background: url('/img/bg.jpg');background-color: transparent;background-repeat: no-repeat;background-position: center;background-size: cover;">
     @yield('content')
   </body>
 </html>
